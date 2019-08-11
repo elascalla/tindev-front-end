86f9f943bd98e9a59179c47965d02cafcc0de612
elascalla <erick.lascalla@gmail.com>
2019-08-11 04:11:57 +0000

    Ajustes frontend

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
 import { Link } from 'react-router-dom';
 import './Main.css';
 
@@ -7,6 +8,7 @@ import api from '../services/api';
 import logo from '../assets/logo.svg';
 import like from '../assets/like.svg';
 import dislike from '../assets/dislike.svg';
+import itsamatch from '../assets/itsamatch.png';
 
 export default function Main({ match }) {
   /* Utilizamos a chamada no botão onClick dessa maneira: onClick={() => handleDislike(user._id)}>, pelo seguinte motivo
@@ -14,6 +16,7 @@ export default function Main({ match }) {
   quando queremos uma chamada exatamente no clique do botão devemos fazer essa hackzinho de colocar uma função chamando a função a ser executada. */
 
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     async function loadUsers() {
@@ -27,6 +30,16 @@ export default function Main({ match }) {
     }
 
     loadUsers();
+  }, [match.params.id]);
+
+  useEffect(() => {
+    const socket = io('http://localhost:3333', {
+      query: { user: match.params.id }
+    });
+
+    socket.on('match', dev => {
+      setMatchDev(dev);
+    })    
   }, [match.params.id]);
 
   async function handleLike(id) {
@@ -73,6 +86,18 @@ export default function Main({ match }) {
         </ul>
       ) : (
         <div className="empty">Acabou :(</div>
+      ) }
+
+      { matchDev && (
+        <div className="match-container">
+          <img src={itsamatch} alt="It's a match" />
+
+          <img className="avatar" src={matchDev.avatar} alt="" />
+          <strong>{matchDev.name}</strong>
+          <p>{matchDev.bio}</p>
+
+          <button type="button" onClick={() => setMatchDev(null)}>FECHAR</button>
+        </div>
       ) }
     </div>
   )
